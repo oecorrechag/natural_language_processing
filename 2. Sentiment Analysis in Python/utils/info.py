f99d0f63481e0f61eaac7f22cee5ea1cc2f3c939
oecorrechag <oecorrechag@unal.edu.co>
2023-04-29 15:44:57 -0500

    2.1.7

---
+++ 2. Sentiment Analysis in Python/utils/info.py
@@ -0,0 +1,6 @@
+
+two_cities = 'It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way – in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only.'
+
+annak = 'Happy families are all alike; every unhappy family is unhappy in its own way'
+
+catcher = "If you really want to hear about it,the first thing you'll probably want to know is where I was born, and what my lousy childhood was like, and how my parents were occupied and all before they had me, and all that David Copperfield kind of crap, but I don't feel like going into it, if you want to know the truth."
