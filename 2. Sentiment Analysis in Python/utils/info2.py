efed4561e5d7a601941c12adab0517eb59828826
oecorrechag <oecorrechag@unal.edu.co>
2023-04-29 17:50:42 -0500

    2.2.11

--- 2. Sentiment Analysis in Python/utils/info2.py
+++ 2. Sentiment Analysis in Python/utils/info2.py
@@ -1,2 +1,11 @@
 
 annak = ['Happy families are all alike;', 'every unhappy family is unhappy in its own way']
+
+GoT = 'Never forget what you are, for surely the world will not. Make it your strength. Then it can never be your weakness. Armour yourself in it, and it will never be used to hurt you.'
+
+avengers = ["Cause if we can't protect the Earth, you can be d*** sure we'll avenge it",
+ 'There was an idea to bring together a group of remarkable people, to see if we could become something more',
+ "These guys come from legend, Captain. They're basically Gods."]
+
+
+ 
